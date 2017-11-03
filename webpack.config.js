fb9545e1726b1a117eaa9a9d896644889e08c4f9
Nikolay <artelbear@gmail.com>
2017-11-03 19:20:57 +0300

    WEBPACKgit add --allgit add --all OH! MY! GAHgit add --all!

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const path = require('path');
+
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.sass$/,
+				use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: ['css-loader', 'sass-loader']
+				})
+			}
+		]
+	},
+	plugins: [
+		new ExtractTextPlugin('style.css')
+	]
+};
