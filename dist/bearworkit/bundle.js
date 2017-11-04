0a441094cb84ff92f0b6d1d3ffe5511f952346fc
Nikolay <artelbear@gmail.com>
2017-11-04 22:30:24 +0300

    M-MAAAAAAARGINgit add --all

--- dist/bearworkit/bundle.js
+++ dist/bearworkit/bundle.js
@@ -84,6 +84,7 @@ $(document).ready(function() {
 	$("#MagicImage").on('load', function() {
 		$("#MagicImageBox").css("height", ($("#MagicImage").height() - 5) + "px");
 		$("#BearLoad").animate({height: "0px"}, 2000, function(){$("#BearLoad").remove()});
+		$("#MagicImageBox").animate({"margin-bottom": "-10em"}, 2000);
 		$(window).scroll(function() {
 			$("#MagicImage").css("top", (($(window).scrollTop() / 4) - 5) + "px");
 		});
