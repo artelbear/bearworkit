519d9cac15e3bc47253659565f7abd94efbcedd1
Nikolay <artelbear@gmail.com>
2017-11-27 10:46:31 +0300

    HIGEST RESTART EVER

--- src/index.js
+++ src/index.js
@@ -1,30 +1,5 @@
 import "./sass/base.sass";
-import "./sass/mobi.sass";
-import Vue from "./js/vue.js";
 
-function vue_oper() {
-	window.scrollTo(0, 0);
+function start() {};
 
-	var loader = new Vue ({
-		el: "#BearLoad",
-		data: {
-			show: true
-		}
-	});
-
-	loader.show = false
-	
-	var Parallax = new Vue ({
-		el: "#MagicImage",
-		data: {
-			paraTop: "0px"
-		}
-	});
-
-	window.onscroll = function() {
-		var scrolled = window.pageYOffset || document.documentElement.scrollTop;
-		Parallax.paraTop = (scrolled / 4) + "px"
-	};
-}; // VUE FUN END
-
-window.onload = vue_oper;
+window.onload = start;
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
+const webpack = require('webpack');
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+require('url-loader')
 
 module.exports = {
   entry: './src/index.js',
@@ -15,10 +17,29 @@ module.exports = {
           fallback: 'style-loader',
           use: ['css-loader', 'sass-loader']
         })
+      },
+      {
+        // Match woff2 and patterns like .woff?v=1.1.1.
+        test: /\.woff2?(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url-loader",
+        options: {
+          limit: 50000,
+          mimetype: "application/font-woff",
+          name: "./fonts/[name].[ext]", // Output below ./fonts
+          publicPath: "./fonts" // Take the directory into account
+        }
       }
     ]
   },
   plugins: [
-    new ExtractTextPlugin('style.css')
+    new ExtractTextPlugin('style.css'),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jquery: "jQuery",
+      "windows.jQuery": "jquery"
+    }),
+    new webpack.ProvidePlugin({
+      Vue: ['vue/dist/vue.esm.js', 'default']
+    })
   ]
 };
